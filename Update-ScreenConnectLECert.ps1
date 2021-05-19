3d957188d3cfb98547b8623e40de10f605d2291e
aboell <andy.boell@gmail.com>
2021-05-19 10:12:29 -0500

    Update Update-ScreenConnectLECert.ps1

--- Update-ScreenConnectLECert.ps1
+++ Update-ScreenConnectLECert.ps1
@@ -2,12 +2,12 @@
 #
 #  ScreenConnect / Let's Encrypt / Cerbot Script
 #
-#  This script takes a recently renewed Let's Encrypt SSL certificate renewed by Certbot
+#  This script takes a Let's Encrypt SSL certificate by Certbot
 #  and applies it to the local Screen Connect server
 #  
 #  Created by Andy Boell, NNNC
 #
-#  Last Updated: 5/18/2021
+#  Last Updated: 5/19/2021
 
 function Logging {
     param([string]$Message)
@@ -15,38 +15,60 @@ function Logging {
     $Message >> $LogFile
 }
 
+# Get Cerbot certificate folder 
+$certDirectoryName = Get-ChildItem -Directory -Path C:\Certbot\live
+
 # Define log file and make initial entry to indicate this script has run
 $LogFile = 'c:\certs\SSLCert.log'
 Logging -Message "Script start $(Get-Date)"
 
 # Test if the certificate has been renewed
-if((Get-ChildItem C:\Certbot\live\connect.esu1.org | ? LastWriteTime -gt (Get-Date).AddDays(-7) | Measure-Object).Count -gt 0)
+if((Get-ChildItem $certDirectoryName.FullName | ? LastWriteTime -gt (Get-Date).AddDays(-7) | Measure-Object).Count -gt 0)
 {
-    Logging -Message "Certificate renewed within the last 7 days"
+    Logging -Message "Certificate renewed within the last 7 days.  Needs to be imported into certificate store and bound to Screen Connect"
     # Create PFX
-    Logging -Message "Create PFX"
+    Logging -Message "Set variables for openssl command"
     $certPath = "c:\certs\connect-$(get-date -f yyyy-MM-dd).pfx"
-    openssl pkcs12 -inkey C:\Certbot\live\connect.esu1.org\privkey.pem -in C:\Certbot\live\connect.esu1.org\cert.pem -certfile C:\Certbot\live\connect.esu1.org\fullchain.pem -export -out $certPath -password pass:connect
+    $privkey = $certDirectoryName.FullName + '\privkey.pem'
+    $cert = $certDirectoryName.FullName + '\cert.pem'
+    $certfile = $certDirectoryName.FullName + '\fullchain.pem'
+    try{
+        Logging -Message "Create PFX"
+        openssl pkcs12 -inkey $privkey -in $cert -certfile $certfile -export -out $certPath -password pass:connect
+        Logging -Message "PFX successfully created: $($certPath)"
+    }
+    catch{
+    }
+
+    # Import certificate into certificate store
+    try{
+        Logging -Message "Import new SSL certificate to LocalMachine\My certificate store"
+        $importedCert = Import-PfxCertificate -FilePath $certPath -CertStoreLocation Cert:\LocalMachine\My -Password ('connect' | ConvertTo-SecureString -AsPlainText -Force)
+        
+        # Verify the certificate was actually imported into the certificate store
+        if((Get-ChildItem -Path Cert:\LocalMachine\My | Where-Object {$_.Thumbprint -eq $importedCert.Thumbprint}).count)
+        {
+            Logging -Message "New SSL certificate successfully imported into certificate store"
+        }
+        else
+        {
+            Logging -Message "Error: SSL certificate not imported into certificate store"
+        }
+    }
+    catch{
+    }
 
     # Delete existing SSL certificate bind to port 443
     Logging -Message "Delete existing SSL certificate bind to port 443"
     netsh http delete sslcert ipport=0.0.0.0:443
 
-    # Remove old certs
-    Logging -Message "Remove old cert from LocalMachine\My certificate store"
-    ls Cert:\LocalMachine\My | ? Subject -eq "CN=connect.esu1.org"  | remove-item -Force
-
-    # Import certificate into certificate store
-    Logging -Message "Import new SSL certificate to LocalMachine\My certificate store"
-    Import-PfxCertificate -FilePath $certPath -CertStoreLocation Cert:\LocalMachine\My -Password ('connect' | ConvertTo-SecureString -AsPlainText -Force)
-
-    # Get new cert
-    Logging -Message "Query certificate store to get the new certificate"
-    $cert = Get-ChildItem -Path Cert:\LocalMachine\My | ? Subject -eq "CN=connect.esu1.org"
-
     # Bind new SSL certificate to port 443
     Logging -Message "Bind new SSL certificate to port 443 for Screen Connect's use"
-    netsh http add sslcert ipport=0.0.0.0:443 certhash=$($cert.Thumbprint) appid="{00000000-0000-0000-0000-000000000000}"
+    netsh http add sslcert ipport=0.0.0.0:443 certhash=$($importedCert.Thumbprint) appid="{00000000-0000-0000-0000-000000000000}"
+    
+    # Remove old certs
+    Logging -Message "Remove old cert from LocalMachine\My certificate store"
+    ls Cert:\LocalMachine\My | ? Subject -eq "CN=$($certDirectoryName.Name)" | ? NotAfter -lt $(get-date) | remove-item -Force
 }
 else
 {
